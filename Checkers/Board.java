ba33c7e182c040883642240ae122e4e3375375e5
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2018-10-03 18:19:05 -0500

    Created Board

--- Checkers/Board.java
+++ Checkers/Board.java
@@ -1,5 +1,14 @@
 package Checkers;
 
 public class Board{
-	Piece[][] board = new Piece[8][8];
+	private Piece[][] board = new Piece[8][8];
+	
+	public setPiece(Piece p, int x, int y){
+		//error if x or y are > 7
+		board[x][y] = p;
+	}
+	
+	public boolean checkOccupied(int x, int y){
+		return (board[x][y] != null);
+	}
 }
